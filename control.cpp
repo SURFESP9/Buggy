080dde85c0a51b6bfc7a68323601c26076cd7281
leikstern <35392686+HaloXena@users.noreply.github.com>
2020-02-20 13:00:06 +0000

    Update control.cpp

--- control.cpp
+++ control.cpp
@@ -6,17 +6,34 @@ const float radius = 0.07;
 
 void getEncoderLeft();
 void getEncoderRight();
+void isLineBroken(float sensorVal,float* prevRspeed,float* prevLspeed)                            //interrupt
+{
+    if(sensorVal <= 100)                    //100 is temporary value. later change with calculated line value
+    {
+         onLineBreak();
+    }
+}
+
 void calSpeedLeft();
 void calSpeedRight();
-void getSensor();
-void speedPID();
-void directionPID();
+int isLineMid(int sensorVal, int* LED)
+{
+    if(sensorVal<=5000 && sensorVal>=3000){
+        LED = 1;
+    }
+}
+void spdPID();
+void dirPID();
 void breakSensing();
 void PID();
 void setMotorSpeedRight();
 void setMotorSpeedLeft();
 
-
+void onLineBreak(float* prevRspeed, float* prevLspeed){            //interrupt (isLineBroken()==1)
+                                                        
+        setMotorSpeedRight(*prevRspeed);
+        setMotorSpeedLeft(*prevLspeed); 
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
